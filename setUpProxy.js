b1c9b9fc2423d4c7c34351e316cf7d5e3ca7880d
antran2704 <phamtrangiaan27@gmail.com>
2022-06-28 22:13:06 +0700

    load code

---
+++ setUpProxy.js
@@ -0,0 +1,12 @@
+const {createProxyMiddleware} = require("http-proxy-middleware")
+
+module.exports = app => {
+    app.use(
+        createProxyMiddleware("/danh-sach/phim-moi-cap-nhat?page=1",
+         {
+            target: "https://ophim1.com",
+            changeOrigin: true
+         }
+        )
+    )
+}
